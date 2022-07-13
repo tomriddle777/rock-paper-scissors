b065ac26685fa3d3db0216ec303974714bf31037
tomriddle777 <tommy.lu112358@gmail.com>
2022-07-13 16:45:14 -0700

    Made the game playable and added 5 rounds
    Added player input, and added a score system that tallied the scores of each of the 5 rounds to determine the final winner.

--- script.js
+++ script.js
@@ -6,33 +6,75 @@ function computerPlay() {
   const num = randomInt(3);
   switch (num) {
     case 0:
-      return "Rock";
+      return "rock";
       break;
     case 1:
-      return "Paper";
+      return "paper";
       break;
     case 2:
-      return "Scissors";
+      return "scissors";
       break;
+    default:
+      return "Computer selection error.";
   }
 }
 
-function playRound (playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   computerSelection = computerSelection.toLowerCase();
   if (playerSelection === computerSelection) {
-    return "Draw.";
+    return 0;
   } else if (playerSelection === "rock" && computerSelection === "paper") {
-    return "You lose.";
+    return -1;
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return "You win.";
+    return 1;
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return "You win.";
+    return 1;
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    return "You lose.";
+    return -1;
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    return "You lose.";
+    return -1;
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return "You win.";
+    return 1;
+  } else {
+    return "--error--";
   }
 }
+
+function game() {
+  finalResult = 0
+  for (let i = 0; i < 5; i++) {
+    let playerChoice = prompt(`Round ${i + 1}\nChoice: `);
+    let computerChoice = computerPlay();
+    console.log(`Round ${i + 1}:\nYou chose ${playerChoice} and the computer chose ${computerChoice}.`);
+    result = playRound(playerChoice, computerChoice);
+    finalResult += result;
+    switch (result) {
+      case 0:
+        console.log("Draw.");
+        break;
+      case 1:
+        console.log("You win!");
+        break;
+      case -1:
+        console.log("You lose.");
+        break;
+      default:
+        console.log("Round error.");
+        break;
+    }
+    console.log(`Points: ${finalResult}.`);
+  }
+  console.log("Final result: ");
+  if (finalResult > 0) {
+    return "You won the game!";
+  } else if (finalResult < 0) {
+    return "You lost the game.";
+  } else if (finalResult === 0) {
+    return "The game is tied.";
+  } else {
+    return "Error.";
+  }
+}
+
+console.log(game());
