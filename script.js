afe0298963f547d6bd167168c84b648fecd6f8e9
tomriddle777 <tommy.lu112358@gmail.com>
2022-07-19 21:37:16 -0700

    Added UI
    Used event listeners to allow users to play the game with buttons.

--- script.js
+++ script.js
@@ -42,40 +42,41 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  finalResult = 0;
-  for (let i = 0; i < 5; i++) {
-    let playerChoice = prompt(`Round ${i + 1}\nChoice: `);
-    let computerChoice = computerPlay();
-    console.log(`Round ${i + 1}:\nYou chose ${playerChoice} and the computer chose ${computerChoice}.`);
-    result = playRound(playerChoice, computerChoice);
-    finalResult += result;
-    switch (result) {
-      case 0:
-        console.log("Draw.");
-        break;
-      case 1:
-        console.log("You win!");
-        break;
-      case -1:
-        console.log("You lose.");
-        break;
-      default:
-        console.log("Round error.");
-        break;
-    }
-    console.log(`Points: ${finalResult}.`);
+function gameRound() {
+  let playerSelection = this.className, computerSelection = computerPlay();
+  let result = playRound(playerSelection, computerSelection);
+  finalScore += result;
+  let textResult = ""
+  switch (result) {
+    case 1:
+      textResult = "You won this round!";
+      break;
+    case -1:
+      textResult = "You lost this round.";
+      break;
+    case 0:
+      textResult = "Tie.";
+      break;
   }
-  console.log("Final result: ");
-  if (finalResult > 0) {
-    return "You won the game!";
-  } else if (finalResult < 0) {
-    return "You lost the game.";
-  } else if (finalResult === 0) {
-    return "The game is tied.";
-  } else {
-    return "Error.";
+  results.textContent = `Result: You chose ${playerSelection} and the computer chose ${computerSelection}. ${textResult}`;
+  score.textContent = `Score: ${finalScore}`;
+  if (finalScore === 5 || finalScore === -5) {
+    if (finalScore === 5) {
+      results.textContent = 'You win the game!';
+    } else if (finalScore === -5) {
+      results.textContent = 'You lost the game.';
+    }
+    finalScore = 0;
   }
 }
 
-console.log(game());
+let finalScore = 0;
+const rock = document.querySelector('.rock'),
+  paper = document.querySelector('.paper'),
+  scissors = document.querySelector('.scissors'),
+  results = document.querySelector('.results'),
+  score = document.querySelector('.score');
+
+rock.addEventListener('click', gameRound);
+paper.addEventListener('click', gameRound);
+scissors.addEventListener('click', gameRound);
